d7573bdf9d2ac3737ad21767ceb7e1a1b4ea25da
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-20 18:05:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, idx)
   i = board[idx]
-  return i = nil || i.empty? || i != " "
+  return (i = nil || i.empty? || i != " ")
 end
